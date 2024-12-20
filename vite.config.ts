3072bd6662df0816dc270177c602b67b35bf3f91
Gourab Mahapatra <mgourab@hotmail.com>
2024-12-20 22:03:45 +0530

    modify vite.config to use tailwind and autoprefixer

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
+import autoprefixer from 'autoprefixer'
+import tailwind from 'tailwindcss'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
   plugins: [vue()],
 })
